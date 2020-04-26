14b350b2dda2f2b592f933d2ed143e27e2053e40
Miles McBain <miles.mcbain@gmail.com>
2020-04-26 20:14:42 +1000

    fix hotkey killer

--- gnome/disable_app_keys.sh
+++ gnome/disable_app_keys.sh
@@ -2,7 +2,5 @@
 for i in {1..9}; do 
   gsettings set "org.gnome.shell.keybindings" "switch-to-application-$i" "[]" 
   gsettings set "org.gnome.shell.extensions.dash-to-dock" "app-hotkey-$i" "[]" 
+  gsettings set "org.gnome.shell.extensions.dash-to-dock" "app-shift-hotkey-$i" "[]" 
 done
-
-gsettings set "org.gnome.shell.extensions.dash-to-dock" "app-hotkey-10" "[]" 
-gsettings set "org.gnome.shell.extensions.dash-to-dock" "app-shift-hotkey-1" "[]" 
